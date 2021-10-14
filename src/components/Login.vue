5dc11bbd6e21ffb28eba342d8f5c28de887563d8
zhangwanli <wanli.zhang@dewmobile.com>
2021-10-14 21:04:15 +0800

    fix login

---
+++ src/components/Login.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="login">
+        <div class="login_box">
+            <!-- 头像去 -->
+            <div class="avatar_box">
+                <img src="../assets/logo.png" alt="">
+            </div>
+            <!-- 登录表单区域 -->
+            <el-form :model='loginForm' :rules="loginFormRules" label-width="0px" class="login_form">
+                <!-- 用户名 -->
+                <el-form-item prop="username">
+                    <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+                </el-form-item>
+                <!-- 密码 -->
+                <el-form-item>
+                    <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
+                </el-form-item>
+
+                <!-- 按钮区域 -->
+                <el-form-item class="btns">
+                    <el-button type="primary">登录</el-button>
+                    <el-button type="info">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      loginForm: {
+        username: '张三',
+        password: '123'
+      },
+      // 表单验证对象
+      loginFormRules: {
+        // 验证用户名是否合法
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '请输入 3 到 10 个字符', trigger: 'blur' }
+        ],
+        // 验证密码是否合法
+        password: []
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login{
+    background-color: #2b4b6b;
+    height: 100%;
+}
+.login_box{
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    width: 450px;
+    height: 300px;
+    background-color: #ffffff;
+    border-radius: 3px;
+}
+.avatar_box {
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    width: 130px;
+    height: 130px;
+    padding: 10px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    box-shadow: 0 0 10px #ddd;
+    background-color: #ffffff;
+    img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+    }
+}
+.btns {
+    display: flex;
+    justify-content: end;
+}
+.login_form {
+    width: 100%;
+    padding: 0 20px;
+    position: absolute;
+    bottom: 0;
+    box-sizing: border-box;
+}
+</style>
