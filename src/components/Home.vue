67f2b3c4a05ece556629da6d551b909d32529fde
zhangwanli <wanli.zhang@dewmobile.com>
2021-10-15 21:37:17 +0800

    添加home整体布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
         <el-button type="info" @click="logout">退出</el-button>
+        <el-container>
+            <el-header>Header</el-header>
+            <el-container>
+                <el-aside width="200px">Aside</el-aside>
+                <el-main>Main</el-main>
+            </el-container>
+        </el-container>
     </div>
 </template>
 
