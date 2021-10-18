da9630d30450f8ac122782da477fbf13725773b2
yinyan <yin.yan01@iwhalecloud.com>
2021-10-18 23:17:33 +0800

    fix some

---
+++ src/components/power/Rights.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <input type="text" v-model="firstName">
+        <input type="text" v-model="lastName">
+        <span>{{computedFullName}}</span>
+        <span>{{computedFullName}}</span>
+        <button @click="computedFullName">计算全名</button>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      firstName: '张',
+      lastName: '万里',
+      fullName: ''
+    }
+  },
+  methods: {
+  },
+  computed: {
+    computedFullName () {
+      console.log('computedFullName')
+      return this.firstName + this.lastName
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
